99a9c71245509c6ccce920812956498083f0ee79
Anh-Quoc <anhquoc5.1.2003.q@gmail.com>
2024-01-25 11:02:06 +0700

    Create PlantService

---
+++ src/main/java/services/PlantService.java
@@ -0,0 +1,45 @@
+package services;
+
+import daos.PlantDAO;
+import entities.Plant;
+
+import java.util.Vector;
+
+public class PlantService {
+
+    private PlantDAO plantDAO;
+
+    public PlantService() {
+        plantDAO = PlantDAO.getInstance();
+    }
+
+    public Vector<Plant> getAllPlant() {
+        Vector<Plant> plants = plantDAO.getAllPlant();
+        return plants;
+    }
+
+    public Vector<Plant> searchPlantByTitle(String productTitle){
+        return plantDAO.getPlantByTitle(productTitle);
+    }
+
+    public Plant getPlantById(int id) {
+        Plant plant = plantDAO.getPlantById(id);
+        return plant;
+    }
+
+    public Vector<Plant> getFeaturedPlants() {
+        Vector<Plant> plants = plantDAO.getFeaturedPlant();
+        return plants;
+    }
+
+    public Vector<Plant> getBestsellerPlants() {
+        Vector<Plant> plants = plantDAO.getBestsellerPlant();
+        return plants;
+    }
+
+    public Vector<Plant> getLatestPlants() {
+        Vector<Plant> plants = plantDAO.getLatestPlant();
+        return plants;
+    }
+
+}
