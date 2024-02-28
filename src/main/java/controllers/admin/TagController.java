233093bffd927f0e8b280e9c093305251c057404
Anh Quoc <anhquoc5.1.2003.q@gmail.com>
2024-02-28 21:53:21 +0700

    feature: view all plant tag

---
+++ src/main/java/controllers/admin/TagController.java
@@ -0,0 +1,70 @@
+package controllers.admin;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import services.PlantTagService;
+
+import java.io.IOException;
+
+@WebServlet(name = "TagController", urlPatterns = "/admin-tags")
+public class TagController extends HttpServlet {
+
+    private PlantTagService plantTagService;
+
+    @Override
+    public void init() throws ServletException {
+        plantTagService = PlantTagService.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String command = req.getParameter("command");
+        switch (command) {
+            case "READ":
+                getAllTag(req, resp);
+                break;
+            case "DELETE":
+                int id = Integer.parseInt(req.getParameter("tagID"));
+//                plantTagService.deleteTagByID(id);
+                getAllTag(req, resp);
+                break;
+            case "SEARCH":
+                String search = req.getParameter("searchValue");
+//                req.setAttribute("listTags", plantTagService.searchTagByName(search));
+                req.getRequestDispatcher("Tag.jsp").forward(req, resp);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void getAllTag(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("listTags", plantTagService.getAllPlantTag());
+        req.getRequestDispatcher("Tag.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String command = req.getParameter("command");
+        switch (command) {
+            case "CREATE":
+                String name = req.getParameter("name");
+//                plantTagService.createNewTag(name);
+                getAllTag(req, resp);
+                break;
+            case "UPDATE":
+                Integer id = Integer.parseInt(req.getParameter("tagID"));
+                String nameUpdate = req.getParameter("name");
+//                plantTagService.updateTagByID(id, nameUpdate);
+                getAllTag(req, resp);
+                break;
+            default:
+                break;
+        }
+
+    }
+}
--- src/main/webapp/admin-index.jsp
+++ src/main/webapp/admin-index.jsp
@@ -6,7 +6,7 @@
 <body>
     <a href="admin-plants?command=READ">Plant</a> <br>
     <a href="categories?command=READ">Plant Categories</a> <br>
-    <a href="tags?command=READ">Plant Tags</a> <br>
+    <a href="admin-tags?command=READ">Plant Tags</a> <br>
     <a href="users?command=READ">Users</a> <br>
     <a href="orders?command=READ">Orders</a> <br>
     <a href="logout">Logout</a>
