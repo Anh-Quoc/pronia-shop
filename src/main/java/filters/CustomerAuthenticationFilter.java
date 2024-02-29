251ce046210801158eb4c8c3cb313a464c61ce77
Anh Quoc <anhquoc5.1.2003.q@gmail.com>
2024-02-29 20:48:19 +0700

    feature: customer authentication filter

---
+++ src/main/java/filters/CustomerAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import services.UserSessionService;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "CustomerAuthenticationFilter",
+        urlPatterns = {"/cart", "/checkout", "/order", "/order-details",
+        "/cart.jsp", "/checkout.jsp", "/my-account.jsp", "/cart.html", "/checkout.html", "/my-account.html"},
+        dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD})
+public class CustomerAuthenticationFilter implements Filter {
+
+    private UserSessionService userSessionService;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        userSessionService = UserSessionService.getInstance();
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // Check cookie has session ID or not
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        Cookie[] cookies = req.getCookies();
+        if(cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("session_id")) {
+                    if (userSessionService.isValidCustomerSessionId(cookie.getValue())) {
+                        filterChain.doFilter(servletRequest, servletResponse);
+                        return;
+                    }
+                }
+            }
+        }
+        res.sendRedirect("login");
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
